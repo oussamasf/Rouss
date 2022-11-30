adc2d1104a195cdcd985a7a2ffa9d9dbb6f6fc2b
Oussama Seffai <Oussama.seffa@legal-doctrine.com>
2022-11-30 13:25:46 +0100

    Better Key Generation

--- src/services/queries/page-cache.ts
+++ src/services/queries/page-cache.ts
@@ -1,14 +1,15 @@
 import {client } from "$services/redis/client"
+import { pageCacheKey } from "$services/keys";
 const cacheRoutes = ["/about","/privacy","/auth/signup","/auth/signin"];
 export const getCachedPage = (route: string) => {
   if (cacheRoutes.includes(route)){
-    return client.get("pageCache#"+route)
+    return client.get(pageCacheKey(route))
   }
   return null;
 };
 export const setCachedPage = (route: string, page: string) => {
   if (cacheRoutes.includes(route)){
-    return client.set("pageCache#"+route , page , {
+    return client.set(pageCacheKey(route) , page , {
       EX: 2
     })
   }
