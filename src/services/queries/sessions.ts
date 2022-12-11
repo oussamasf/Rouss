048e37cb7ca3669bf64d745430ba85b7914a1b25
ousf <ousf@mail.com>
2022-12-11 21:39:15 +0100

    Fetch a Saved Session

--- src/services/queries/sessions.ts
+++ src/services/queries/sessions.ts
@@ -1,5 +1,19 @@
 import type { Session } from '$services/types';
+import { sessionsKey } from '$services/keys';
+import { client } from '$services/redis';
 
-export const getSession = async (id: string) => {};
+export const getSession = async (id: string) => {
+	const session = await client.hGetAll(sessionsKey(id));
+	if (Object.keys(session).length === 0) return null;
+	return deserialize(id, session);
+};
 
 export const saveSession = async (session: Session) => {};
+
+const deserialize = (id: string, session: { [key: string]: string }) => {
+	return {
+		id,
+		username: session.username,
+		password: session.password
+	};
+};
